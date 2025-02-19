cd31ac7a306c2b087201a1a14be7b40fc3a6617e
ZJTJensen <ZJTJensen@yahoo.com>
2025-02-18 19:14:46 -0500

    feat: added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+// filepath: /c:/Users/ZJTJe/Documents/fab-2/fab-odyssey-front-2/src/app/app.routes.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
+import { FabMainComponent } from './fab-main/fab-main.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full', canActivate: [AuthGuard] },
+  { path: 'home', component: FabMainComponent, canActivate: [AuthGuard] },
+  // other routes
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
